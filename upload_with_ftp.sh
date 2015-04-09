a1ef2eb96676adb132faec7b4d28b67819a91e4a
Bert Frees <bertfrees@gmail.com>
2015-04-09 20:44:42 +0200

    Generate site for each branch and tag

--- upload_with_ftp.sh
+++ upload_with_ftp.sh
@@ -1,11 +1,17 @@
 #!/usr/bin/env bash
 set -x
 set -e
+if [ -n "$TRAVIS_PULL_REQUEST" ] && ! [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
+    echo "Skipping pull request"
+    exit 0
+fi
+BRANCH=$TRAVIS_TAG
+[ -z "$BRANCH" ] && BRANCH=$TRAVIS_BRANCH
+[ -z "$BRANCH" ] && read -p "branch: " BRANCH
 FTP_HOST=ftp://ftpcluster.loopia.se
-[ -z "$FTP_USER" ] && read -p "user: " FTP_USER
-[ -z "$FTP_SECRET" ] && read -s -p "secret: " FTP_SECRET
+[ -z "$FTP_USER" ] && read -p "ftp user: " FTP_USER
+[ -z "$FTP_SECRET" ] && read -s -p "ftp secret: " FTP_SECRET
 shopt -s globstar
 for file in target/site/**/*; do
-    [ -f $file ] && curl --ftp-create-dirs -T $file $FTP_HOST/${file#target/site/} --user $FTP_USER:$FTP_SECRET
+    [ -f $file ] && curl --ftp-create-dirs -T $file $FTP_HOST/$BRANCH/${file#target/site/} --user $FTP_USER:$FTP_SECRET
 done
-
