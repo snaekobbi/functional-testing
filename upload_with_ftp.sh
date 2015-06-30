e2ac6bc7d685fa3fc93639d9f39575e77c04a515
Bert Frees <bertfrees@gmail.com>
2015-06-30 19:00:51 +0200

    Enhance upload_with_ftp script

--- upload_with_ftp.sh
+++ upload_with_ftp.sh
@@ -7,10 +7,14 @@ if [ -n "$TRAVIS_PULL_REQUEST" ] && ! [ "$TRAVIS_PULL_REQUEST" == "false" ]; the
 fi
 BRANCH=$TRAVIS_TAG
 [ -z "$BRANCH" ] && BRANCH=$TRAVIS_BRANCH
-[ -z "$BRANCH" ] && read -p "branch: " BRANCH
+if [ -z "$BRANCH" ]; then
+    DEFAULT_BRANCH=$( git rev-parse --abbrev-ref HEAD )
+    read -p "branch? ($DEFAULT_BRANCH) " BRANCH
+    [ -z "$BRANCH" ] && BRANCH=$DEFAULT_BRANCH
+fi
 FTP_HOST=ftp://ftpcluster.loopia.se
-[ -z "$FTP_USER" ] && read -p "ftp user: " FTP_USER
-[ -z "$FTP_SECRET" ] && read -s -p "ftp secret: " FTP_SECRET
+[ -z "$FTP_USER" ] && read -p "ftp user? " FTP_USER
+[ -z "$FTP_SECRET" ] && read -s -p "ftp secret? " FTP_SECRET
 shopt -s globstar
 for file in target/site/**/*; do
     [ -f "$file" ] && curl --ftp-create-dirs -T "$file" "$FTP_HOST/$BRANCH/${file#target/site/}" --user $FTP_USER:$FTP_SECRET
